fc1d9dbc86000c7b84b6e4ac171e6b62dcf7739d
takashi nakamura <polluxtk@gmail.com>
2018-07-18 18:26:45 +0900

    Initial commit.

---
+++ Assets/Scale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scale : MonoBehaviour {
+    public GameObject targetGo;
+    public Slider scaleSlider;
+
+    public void Changed () {
+        float scale = scaleSlider.value;
+        targetGo.transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
